2f06ff7dd646b08e6fe59f31ebcf0e77415c6f2a
Alessandro Bollini <alessandro.bollini@metreeca.com>
2022-10-23 00:18:51 +0200

    Improve page loading in default template

--- src/main/resources/com/metreeca/mark/files/index.js
+++ src/main/resources/com/metreeca/mark/files/index.js
@@ -16,50 +16,55 @@
 
 (function (window) {
 
-	//// Side Toggling /////////////////////////////////////////////////////////////////////////////////////////////////
-
-	window.onhashchange=function () {
-		window.document.getElementById("toggle").checked=false;
-	};
-
 
 	//// browser-specific css selectors (eg html[data-useragent*='MSIE 10.0']) /////////////////////////////////////////
 
 	document.documentElement.setAttribute("data-useragent", navigator.userAgent);
 
 
+	//// Side Toggling /////////////////////////////////////////////////////////////////////////////////////////////////
+
+	window.addEventListener("hashchange", function () {
+		window.document.getElementById("toggle").checked=false;
+	});
+
+
 	//// ;) ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	(function (a) {
+	window.addEventListener("load", function () {
 
-		for (var i=0; i < a.length; ++i) {
-			a[i]=255-a[i];
-		}
+		(function (a) {
 
-		var b=String.fromCharCode.apply(String, a);
-		var c=b.substr(0, 7);
-		var d=b.substr(7);
-
-		var _=document.querySelectorAll("main a");
-
-		for (var i=0; i < _.length; ++i) {
-
-			var e=(_[i].getAttribute("href") || "").match(/^#@([-\w]+)(.*)$/);
-
-			if (e) {
-
-				var f=e[1] || "";
-				var g=e[2] || "";
-
-				_[i].setAttribute("href", c+f+d+g);
-
-				if (_[i].childNodes.length === 0) {
-					_[i].appendChild(document.createTextNode(f+d));
-				}
-
+			for (var i=0; i < a.length; ++i) {
+				a[i]=255-a[i];
 			}
-		}
 
-	})([146, 158, 150, 147, 139, 144, 197, 191, 146, 154, 139, 141, 154, 154, 156, 158, 209, 156, 144, 146]);
+			var b=String.fromCharCode.apply(String, a);
+			var c=b.substr(0, 7);
+			var d=b.substr(7);
+
+			var _=document.querySelectorAll("main a");
+
+			for (var i=0; i < _.length; ++i) {
+
+				var e=(_[i].getAttribute("href") || "").match(/^#@([-\w]+)(.*)$/);
+
+				if (e) {
+
+					var f=e[1] || "";
+					var g=e[2] || "";
+
+					_[i].setAttribute("href", c+f+d+g);
+
+					if (_[i].childNodes.length === 0) {
+						_[i].appendChild(document.createTextNode(f+d));
+					}
+
+				}
+			}
+
+		})([146, 158, 150, 147, 139, 144, 197, 191, 146, 154, 139, 141, 154, 154, 156, 158, 209, 156, 144, 146]);
+
+	});
 
 })(window);
